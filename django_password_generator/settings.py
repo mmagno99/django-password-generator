39ebee1ef0cd2b52bc30f975feed38ffe29f52ce
MagnoDev <magno-god64@outlook.com>
2023-02-24 23:08:21 -0600

    debug false

--- django_password_generator/settings.py
+++ django_password_generator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%u-%rgpa$d#w^wd@^od-qgi^u5@+jm&u279f-rzp0ze=6%wgze'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
